bbba6be7940fdd324f127a5aed7c303a5cb4a664
Nancy Quinonez <nancyq29@hotmail.com>
2025-04-24 20:59:25 -0500

    completed book and single book components

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,1 +1,42 @@
 /* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
+
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+function Books(){
+    const [allBooks, setAllBooks] = useState([])
+
+    const navigate = useNavigate()
+    const handleClick = (book) => {
+        navigate(`/single/${book.id}`)
+    }
+
+    useEffect(()=>{
+        const getAllBooks = async() =>{
+            const res = await fetch("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books")
+            const data = await res.json()
+            setAllBooks(data)
+            console.log(data)
+        }
+        getAllBooks();
+    }, [])
+    
+
+    return(
+        <>
+        {
+            allBooks && (
+                allBooks.map((book)=>(
+                    <div key={book.id}>
+                        <h2>{book.title}</h2>
+                        <img src={book.coverimage} style={{height:'400px'}}/>
+                        <button onClick={()=>handleClick(book)}>Details</button>
+                    </div>
+                ))
+            )
+        }
+        </>
+    )
+}
+
+export default Books
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,1 +1,44 @@
 /* TODO - add your code to create a functional React component that renders details for a single book. Fetch the book data from the provided API. You may consider conditionally rendering a 'Checkout' button for logged in users. */
+
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+
+function SingleBook(){
+    const [loneBook, setLoneBook] = useState([]);
+
+    const { id } = useParams();
+
+
+    useEffect(()=>{
+        const getSingleBook = async() =>{
+            try{
+            const res = await fetch(`https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books/${id}`);
+            const data = await res.json();
+            setLoneBook(data);
+            console.log(data)
+
+        } catch(err){
+            console.error(err);
+        }};
+        getSingleBook();
+
+    }, [id]);
+
+    return(
+    // add classnames to each attribute to help stlye l8r
+
+        <div className="singleBookContainer">
+
+            <img src={loneBook.coverimage}/>
+
+            <h1>{loneBook.title}</h1>
+
+            <h2>{loneBook.author}</h2>
+
+            <p>{loneBook.description}</p>
+        </div>
+    )
+}
+
+export default SingleBook
